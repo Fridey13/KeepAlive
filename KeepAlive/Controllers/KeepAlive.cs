05e520ed6ef423f3f17b1822416d81f64fea5b7f
Friday13 <viktorz@wizardsdev.com>
2021-06-25 13:21:26 +0300

    initial

---
+++ KeepAlive/Controllers/KeepAlive.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KeepAlive.Controllers
+{
+    [ApiController]
+    public class KeepAlive : ControllerBase
+    {
+        [HttpGet, Route("keep-alive")]
+        public IActionResult KeepAliveGet()
+        {
+            return Ok();
+        }
+    }
+}
